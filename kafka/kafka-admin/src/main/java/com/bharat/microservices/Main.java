212431e912838d4fa02637106a73ee4886875eb4
Bharat V <bindian0509@gmail.com>
2023-05-29 23:59:59 +0530

    added kafka -> kafka-admin  sub module

---
+++ kafka/kafka-admin/src/main/java/com/bharat/microservices/Main.java
@@ -0,0 +1,12 @@
+package com.bharat.microservices;
+
+
+/*
+ * @author Bharat V. <bindian0509@gmail.com>
+ * @created Thursday, 31 August 2023
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
