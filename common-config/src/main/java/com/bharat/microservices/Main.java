1190ba2cfbaed007fd58a90a231e6534c07dbc55
Bharat V <bindian0509@gmail.com>
2023-06-02 23:59:59 +0530

    Creating kafka-admin module - Part 1: Configuration and dependencies

---
+++ common-config/src/main/java/com/bharat/microservices/Main.java
@@ -0,0 +1,12 @@
+package com.bharat.microservices;
+
+
+/*
+ * @author Bharat V. <bindian0509@gmail.com>
+ * @created Saturday, 02 September 2023
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
