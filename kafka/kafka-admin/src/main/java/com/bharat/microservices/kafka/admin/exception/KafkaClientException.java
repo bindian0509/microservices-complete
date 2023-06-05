41fcbfb3654d6a78fd905494c83f316e4e477604
Bharat V <bindian0509@gmail.com>
2023-06-05 23:59:59 +0530

    Creating kafka-admin module - Part 1: Configuration and dependencies V

---
+++ kafka/kafka-admin/src/main/java/com/bharat/microservices/kafka/admin/exception/KafkaClientException.java
@@ -0,0 +1,7 @@
+package com.bharat.microservices.kafka.admin.exception;
+/*
+* @author Bharat V. <bindian0509@gmail.com>
+* @created Saturday, 02 September 2023
+*/
+public class KafkaClientException {
+}
