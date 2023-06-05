0f00285e9c33dc4474598a4668c34674c76d3f39
Bharat V <bindian0509@gmail.com>
2023-06-05 23:59:59 +0530

    Creating kafka-admin module - Part 1: Configuration and dependencies VI

--- kafka/kafka-admin/src/main/java/com/bharat/microservices/kafka/admin/client/KafkaAdminClient.java
+++ kafka/kafka-admin/src/main/java/com/bharat/microservices/kafka/admin/client/KafkaAdminClient.java
@@ -1,7 +1,189 @@
 package com.bharat.microservices.kafka.admin.client;
+
 /*
-* @author Bharat V. <bindian0509@gmail.com>
-* @created Saturday, 02 September 2023
-*/
+ * @author Bharat V. <bindian0509@gmail.com>
+ * @created Saturday, 02 September 2023
+ */
+
+
+import com.bharat.microservices.config.KafkaConfigData;
+import com.bharat.microservices.config.RetryConfigData;
+import com.bharat.microservices.kafka.admin.exception.KafkaClientException;
+
+import reactor.core.publisher.Mono;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.CreateTopicsResult;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.admin.TopicListing;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.retry.RetryContext;
+import org.springframework.retry.support.RetryTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+
+@Component
 public class KafkaAdminClient {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaAdminClient.class);
+
+    private final KafkaConfigData kafkaConfigData;
+
+    private final RetryConfigData retryConfigData;
+
+    private final AdminClient adminClient;
+
+    private final RetryTemplate retryTemplate;
+
+    private final WebClient webClient;
+
+
+    public KafkaAdminClient(KafkaConfigData config,
+                            RetryConfigData retryConfigData,
+                            AdminClient client,
+                            RetryTemplate template,
+                            WebClient webClient) {
+        this.kafkaConfigData = config;
+        this.retryConfigData = retryConfigData;
+        this.adminClient = client;
+        this.retryTemplate = template;
+        this.webClient = webClient;
+    }
+
+    public void createTopics() {
+        CreateTopicsResult createTopicsResult;
+        try {
+            createTopicsResult = retryTemplate.execute(this::doCreateTopics);
+            LOG.info("Create topic result {}", createTopicsResult.values().values());
+        } catch (Throwable t) {
+            throw new KafkaClientException("Reached max number of retry for creating kafka topic(s)!", t);
+        }
+        checkTopicsCreated();
+    }
+
+    public void checkTopicsCreated() {
+        Collection<TopicListing> topics = getTopics();
+        int retryCount = 1;
+        Integer maxRetry = retryConfigData.getMaxAttempts();
+        int multiplier = retryConfigData.getMultiplier().intValue();
+        Long sleepTimeMs = retryConfigData.getSleepTimeMs();
+        for (String topic : kafkaConfigData.getTopicNamesToCreate()) {
+            while (!isTopicCreated(topics, topic)) {
+                checkMaxRetry(retryCount++, maxRetry);
+                sleep(sleepTimeMs);
+                sleepTimeMs *= multiplier;
+                topics = getTopics();
+            }
+        }
+    }
+
+    public void checkSchemaRegistry() {
+        int retryCount = 1;
+        Integer maxRetry = retryConfigData.getMaxAttempts();
+        int multiplier = retryConfigData.getMultiplier().intValue();
+        Long sleepTimeMs = retryConfigData.getSleepTimeMs();
+        while (!getSchemaRegistryStatus().is2xxSuccessful()) {
+            checkMaxRetry(retryCount++, maxRetry);
+            sleep(sleepTimeMs);
+            sleepTimeMs *= multiplier;
+        }
+    }
+
+    private HttpStatus getSchemaRegistryStatus() {
+        Mono<HttpStatus> responseStatusMono = webClient
+            .get()
+            .uri(kafkaConfigData.getSchemaRegistryUrl())
+            .retrieve()
+            .toBodilessEntity()
+            .map(response -> (HttpStatus) response.getStatusCode());
+
+        AtomicReference<HttpStatus> status = new AtomicReference<>(HttpStatus.SERVICE_UNAVAILABLE);
+        // Subscribe to the response status Mono
+        responseStatusMono.subscribe(status::set);
+        return status.get();
+    }
+
+
+    private void sleep(Long sleepTimeMs) {
+        try {
+            Thread.sleep(sleepTimeMs);
+        } catch (InterruptedException e) {
+            throw new KafkaClientException("Error while sleeping for waiting new created topics!!");
+        }
+    }
+
+    private void checkMaxRetry(int retry, Integer maxRetry) {
+        if (retry > maxRetry) {
+            throw new KafkaClientException("Reached max number of retry for reading kafka topic(s)!");
+        }
+    }
+
+    private boolean isTopicCreated(Collection<TopicListing> topics, String topicName) {
+        if (topics == null) {
+            return false;
+        }
+        return topics.stream().anyMatch(topic -> topic.name().equals(topicName));
+    }
+
+    private CreateTopicsResult doCreateTopics(RetryContext retryContext) {
+        List<String> topicNames = kafkaConfigData.getTopicNamesToCreate();
+        LOG.info("Creating {} topics(s), attempt {}", topicNames.size(), retryContext.getRetryCount());
+        List<NewTopic> kafkaTopics = topicNames.stream().map(topic -> new NewTopic(
+                topic.trim(),
+                kafkaConfigData.getNumOfPartitions(),
+                kafkaConfigData.getReplicationFactor()
+        )).collect(Collectors.toList());
+        return adminClient.createTopics(kafkaTopics);
+    }
+
+    private Collection<TopicListing> getTopics() {
+        Collection<TopicListing> topics;
+        try {
+            topics = retryTemplate.execute(this::doGetTopics);
+        } catch (Throwable t) {
+            throw new KafkaClientException("Reached max number of retry for reading kafka topic(s)!", t);
+        }
+        return topics;
+    }
+
+    private Collection<TopicListing> doGetTopics(RetryContext retryContext)
+            throws ExecutionException, InterruptedException {
+        LOG.info("Reading kafka topic {}, attempt {}",
+                kafkaConfigData.getTopicNamesToCreate().toArray(), retryContext.getRetryCount());
+        Collection<TopicListing> topics = adminClient.listTopics().listings().get();
+        if (topics != null) {
+            topics.forEach(topic -> LOG.debug("Topic with name {}", topic.name()));
+        }
+        return topics;
+    }
+
+
+    public static void main(String[] args) {
+        WebClient webClient = WebClient.create();
+        // Send a GET request to the URL and retrieve the response
+        Mono<HttpStatus> responseStatusMono = webClient
+                .get()
+                .uri("http://localhost:8081")
+                .retrieve()
+                .toBodilessEntity()
+                .map(response -> (HttpStatus) response.getStatusCode());
+
+        // Subscribe to the response status Mono
+        responseStatusMono.subscribe(status -> {
+            System.out.println("HTTP Status Code: " + status.value());
+            if (status == HttpStatus.OK) {
+                System.out.println("The URL is reachable.");
+            } else {
+                System.out.println("The URL is not reachable.");
+            }
+        });
+    }
 }
--- kafka/kafka-admin/src/main/java/com/bharat/microservices/kafka/admin/exception/KafkaClientException.java
+++ kafka/kafka-admin/src/main/java/com/bharat/microservices/kafka/admin/exception/KafkaClientException.java
@@ -1,7 +1,20 @@
 package com.bharat.microservices.kafka.admin.exception;
+
 /*
-* @author Bharat V. <bindian0509@gmail.com>
-* @created Saturday, 02 September 2023
-*/
-public class KafkaClientException {
+ * @author Bharat V. <bindian0509@gmail.com>
+ * @created Saturday, 02 September 2023
+ */
+public class KafkaClientException extends RuntimeException {
+
+    public KafkaClientException() {
+    }
+
+    public KafkaClientException(String message) {
+        super(message);
+    }
+
+    public KafkaClientException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
+
