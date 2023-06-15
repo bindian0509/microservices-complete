5fe3fc8a67bd4f9c2ac5dc79a29d2b8f84e4d1d8
Bharat V <bindian0509@gmail.com>
2023-06-15 21:06:20 +0530

    config server and repo - initialized

---
+++ config-server/src/main/java/com/bharat/microservices/Main.java
@@ -0,0 +1,12 @@
+package com.bharat.microservices;
+
+
+/*
+ * @author Bharat V. <bindian0509@gmail.com>
+ * @created Wednesday, 06 September 2023
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
