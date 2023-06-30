dbb48c7fe8db12db3b3aead44acf3b47a8d8fb01
Bharat V <bindian0509@gmail.com>
2023-06-30 23:59:59 +0530

    Inclusion of encrypt and decrypt endpoints for config server

---
+++ config-server/src/main/java/com/bharat/microservices/config/server/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.bharat.microservices.config.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+/*
+ * @author Bharat V. <bindian0509@gmail.com>
+ * @created Wednesday, 13 September 2023
+ */
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/encrypt/**", "/decrypt/**");
+    }
+
+}
+
